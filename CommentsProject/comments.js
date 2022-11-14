683d7ea9c789978555d2c4f202530786dc14aee9
19Asif30 <19asifani30@gmail.com>
2022-11-14 19:37:01 +0530

    last update

--- CommentsProject/comments.js
+++ CommentsProject/comments.js
@@ -1,3 +1,4 @@
+localStorage.setItem("comments",'[]')
 function addComment(){
     let user_name = document.getElementById("username")
     let comments_1 = document.getElementById("comments")
@@ -8,9 +9,10 @@ function addComment(){
     let element = `<div class="div1">
                         <p id = "title" class = "divitems">${user_name.value}</p>
                         <p id = "para" class = "divitems">${comments_1.value}</p>
-                        <p id = "like" class = "divitems"><span id = "likecount">0 </span> likes<i onclick = "likefunc(this);" class = "fa fa-thumbs-up likebtn"></i><span id = "dislikecount">0</span> dislikes<i onclick = "dislikefunc(this);" class = "fa fa-thumbs-down likebtn"></i> <span id = "del"><i onclick = "del(this);" class = "fa fa-trash"></i></span></p>
+                        <p id = "like" class = "divitems"><span>0</span> likes<i onclick = "likefunc(this);" class = "fa fa-thumbs-up likebtn"></i><span>0</span> dislikes<i onclick = "dislikefunc(this);" class = "fa fa-thumbs-down likebtn"></i> <span id = "del"><i onclick = "del(this);" class = "fa fa-trash"></i></span></p>
                     </div> `
     document.querySelector(".divOuter").innerHTML += element
+
     let arr = JSON.parse(localStorage.getItem("comments"))
     let obj = {
         name: user_name.value,
@@ -20,8 +22,8 @@ function addComment(){
     }
     arr.push(obj)
     localStorage.setItem("comments", JSON.stringify(arr))
-
-    
+    user_name.value = ""
+    comments_1.value = ""
 }
 
 function likefunc(i){
@@ -31,10 +33,8 @@ function likefunc(i){
     const uname = p.parentNode.children[0].innerHTML
     let arr = JSON.parse(localStorage.getItem("comments"))
     for(obj of arr){
-        if(obj.name == uname){
+        if(obj.name == uname)
             obj.likes += 1
-        }
-            
     }
     localStorage.setItem("comments", JSON.stringify(arr))
 }
@@ -61,5 +61,5 @@ function del(ob){
         }
     }
     localStorage.setItem("comments", JSON.stringify(arr))
-
+    ob.parentNode.parentNode.parentNode.remove()
 }
